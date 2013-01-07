a98cde02853eaa66f431b72060b173b3364968bc
Niklas Hedlund <nojan1989@gmail.com>
2013-01-07 19:15:31 +0100

    Revert "Working with graphics"
    This reverts commit f57f86ca37d6b3040b1aed266cc684343f2a5e77.

--- run.py
+++ run.py
@@ -10,39 +10,24 @@ init()
 screen = display.set_mode((640,480))
 run = True
 
-shipImages = [ image.load("ship-%i.png" % i).convert() for i in range(0,3) ]
-for i,s in enumerate(shipImages):
-    shipImages[i].set_colorkey((0,0,255))
-
-animCounter = 0
-doAnimate = False
-
 while run:
     screen.fill((0,0,0))
 
     screen.fill((255,255,255),(0,475,640,5))
-    screen.fill((255,0,0),(300,475,40,5))
+    screen.fill((255,0,0),(310,475,20,5))
 
     pos[0] += speed[0]
     pos[1] += speed[1]
     
-    if pos[1] > (475 - 20):
-        if pos[0] >= 300 and pos[0] <= 320 and speed[1] < 5:
+    if pos[1] > (480 - 10):
+        if pos[0] >= 310 and pos[0] <= 320 and speed[1] < 5:
             print("Successfull landing")
         else:
             print("You crashed!!")
 
-        pos[1] = 455
         run = False
-    
-    if doAnimate:
-        animCounter += 1
-        screen.blit(shipImages[1 + int(animCounter / 3)], (pos[0], pos[1], 20, 20))
-
-        if animCounter == 5:
-            animCounter = 0
     else:
-        screen.blit(shipImages[0], (pos[0], pos[1], 20, 20))
+        screen.fill((0,0,255), (pos[0],pos[1],10,10))
 
     for e in event.get():
         if e.type == QUIT:
@@ -50,18 +35,14 @@ while run:
 
     keys = key.get_pressed()
 
-
-    doAnimate = True
     if keys[K_UP]:
         speed[1] -= THRUSTER
-        #print("Thruster up")
+        print("Thruster up")
     elif keys[K_LEFT]:
         speed[0] -= THRUSTER
-        #print("Thruster left")
+        print("Thruster left")
     elif keys[K_RIGHT]:
         speed[0] += THRUSTER
-    else:
-        doAnimate = False
 
     speed[1] += GRAVITY
 
